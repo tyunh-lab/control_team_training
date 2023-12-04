8d8ca48d157f47dc582814d8ea9a7f152df558ef
tyunh-lab <tyunhs@gmail.com>
2023-12-05 00:21:53 +0900

    分割数を考慮せずに解が導出されてしまう問題の解決

--- abe8.c
+++ abe8.c
@@ -6,16 +6,15 @@ int main()
     double r = 2.0; // 半径
     double s = 0.0; // 面積
 
-    double i = -1 * r; // xの値
+    double i = -1 * r;   // xの値
+    double split = 1000; // 分割数
     while (i < r)
     {
         // y=√(1-x^2)を求める
-        printf("s = %lf\n", sqrt(pow(r, 2) - pow(i, 2)));
         s += sqrt(pow(r, 2) - pow(i, 2));
-        i = i + 0.001;
+        i = i + (1 / split);
     }
-
+    s = s / split;
     printf("s = %f\n", s * 2);
     return 0;
 }
-
